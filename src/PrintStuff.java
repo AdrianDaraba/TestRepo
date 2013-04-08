e4cb95479222db2ddc6d45a5552626ac4cae0e94
AdrianDaraba <adrian.daraba26@gmail.com>
2013-04-08 23:24:54 +0300

    update PrintStuff.java

--- src/PrintStuff.java
+++ src/PrintStuff.java
@@ -1,9 +1,9 @@
-
 public class PrintStuff {
 
 	public static void main(String[] args){
 		
-		System.out.println("1 + 1 = 3");
+		System.out.println("1 + 1 = 2");
 		System.out.println("2 + 1 = 3");
+		System.out.println("1 + 4 = 8");
 	}
 }
